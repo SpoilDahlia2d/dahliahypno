223cc03273fd92c98e99b9132350de81022b7296
SpoilDahlia2d <dahliastar1998@gmail.com>
2025-04-03 20:21:13 +0200

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-// Aggiungi vibrazione su click (funziona su dispositivi mobili)
-document.body.addEventListener("click", function() {
-    navigator.vibrate([100, 100, 100]);  // Vibrazione breve
-});
